a57d367677226b9fbdf0d7087e92ff932686284b
s1140529-sketch <s1140529@dmhs.kh.edu.tw>
2026-05-04 09:38:48 +0800

    Create logic.cpp

---
+++ logic.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a=8;
+    int b=3;
+    int c=a-b;
+    cout<<(a < b)<< " ";
+    cout<<(a >= b)<< " ";
+    cout<<(a >= c)<< " ";
+    cout<<(b < c)<< " ";
+    cout<<(b == b)<< " ";
+    cout<<(a != c)<< " ";
+  
+    cout << "x1 (5+3)  = " << x1 << endl;
+    cout << "x2 (5-3)  = " << x2 << endl;
+    cout << "x3 (5*3)  = " << x3 << endl;
+    cout << "x4 (5/3)  = " << x4 << endl;
+    cout << "x5 (5/3.0)= " << x5 << endl;
+    cout << "x6 (5%3)  = " << x6 << endl;
+
+    return 0;
+}
